c92c76c3cf5f781e869c2b1bf98f49c0d28e2028
damu <wjl@126.com>
2020-02-22 02:11:53 +0800

    vue for bj190919 first commit

---
+++ pre/05_vue-source/MVVM/mvvm.js
@@ -0,0 +1,33 @@
+function MVVM(options) {
+    this.$options = options;
+    var data = this._data = this.$options.data;
+    var me = this;
+
+    Object.keys(data).forEach(function(key) {
+        me._proxy(key);
+    });
+
+    observe(data, this);
+
+    this.$compile = new Compile(options.el || document.body, this)
+}
+
+MVVM.prototype = {
+    $watch: function(key, cb, options) {
+        new Watcher(this, key, cb);
+    },
+
+    _proxy: function(key) {
+        var me = this;
+        Object.defineProperty(me, key, {
+            configurable: false,
+            enumerable: true,
+            get: function proxyGetter() {
+                return me._data[key];
+            },
+            set: function proxySetter(newVal) {
+                me._data[key] = newVal;
+            }
+        });
+    }
+};
---
+++ pre/05_vue-source/MVVM/watcher.js
@@ -0,0 +1,42 @@
+function Watcher(vm, exp, cb) {
+    this.cb = cb;
+    this.vm = vm;
+    this.exp = exp;
+    this.depIds = {};
+    this.value = this.get();
+}
+
+Watcher.prototype = {
+    update: function() {
+        this.run();
+    },
+    run: function() {
+        var value = this.get();
+        var oldVal = this.value;
+        if (value !== oldVal) {
+            this.value = value;
+            this.cb.call(this.vm, value, oldVal);
+        }
+    },
+    addDep: function(dep) {
+        if (!this.depIds.hasOwnProperty(dep.id)) {
+            dep.addSub(this);
+            this.depIds[dep.id] = dep;
+        }
+    },
+    get: function() {
+        Dep.target = this;
+        var value = this.getVMVal();
+        Dep.target = null;
+        return value;
+    },
+
+    getVMVal: function() {
+        var exp = this.exp.split('.');
+        var val = this.vm._data;
+        exp.forEach(function(k) {
+            val = val[k];
+        });
+        return val;
+    }
+};
