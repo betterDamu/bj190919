08eef96b2542e122f2269bcb82ab99792c19f223
damu <wjl@126.com>
2020-02-26 18:57:07 +0800

    0226 -4

---
+++ work/day04/todolist/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <div id="app">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <todo-header></todo-header>
+        <todo-list :listArr="listArr"></todo-list>
+        <todo-footer></todo-footer>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import todoHeader from "./components/todoHeader";
+  import todoList from "./components/todoList";
+  import todoFooter from "./components/todoFooter";
+  export default {
+    name: 'App',
+    data(){
+      return {
+        listArr:[
+          {id:0,content:"98k",checked:false},
+          {id:1,content:"三级头",checked:false},
+          {id:2,content:"三级甲",checked:false}
+        ]
+      }
+    },
+    components:{
+      "todo-header":todoHeader,
+      "todo-list":todoList,
+      "todo-footer":todoFooter
+    }
+  }
+</script>
+
+<style scoped>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
---
+++ work/day04/todolist/src/components/todoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="todo-main">
+    <todo-item v-for="item in listArr" :item="item"></todo-item>
+  </ul>
+</template>
+
+<script>
+    import todoItem from "./todoItem"
+    export default {
+        name: "todoList",
+        props:{
+          listArr:Array
+        },
+        components:{
+          "todo-item":todoItem
+        }
+    }
+</script>
+
+<style scoped>
+  .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
