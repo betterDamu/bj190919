2adb6d1a975afda7e5a0d3d8c562476b4e092dac
damu <wjl@126.com>
2020-03-02 14:37:43 +0800

    0302 -4

---
+++ work/day07/ajax-demo/src/components/getAxios.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    测试axios的get方法
+  </div>
+</template>
+
+<script>
+    //https://api.github.com  github的请求的基地址
+
+    //在js中函数是一等公民   它也是个对象
+    // 1. axios(config)
+    // 2. axios(url,config)
+    // 3. axios.get(url,config)
+
+
+    import axios from "axios";
+    export default {
+        name: "getAxios",
+        async mounted(){
+          const body = await axios({
+            url:"/repos/betterDamu/bj190919/issues",
+            method:"get",
+            baseURL: 'https://api.github.com'
+          })
+
+          /*const body = await axios("https://api.github.com/repos/betterDamu/bj190919/issues",{
+            method:"get"
+          })*/
+
+          // const body = await axios.get("https://api.github.com/repos/betterDamu/bj190919/issues")
+          console.log(body.data[0],"+++++")
+        }
+    }
+</script>
+
+
+<style scoped>
+
+</style>
--- work/day07/ajax-demo/src/router/index.js
+++ work/day07/ajax-demo/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import getAxios from '@/components/getAxios'
+import postAxios from '@/components/postAxios'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {path: '/', name: 'HelloWorld', component: HelloWorld},
+    {path: '/getAxios', name: 'getAxios', component: getAxios},
+    {path: '/postAxios', name: 'postAxios', component: postAxios}
   ]
 })
